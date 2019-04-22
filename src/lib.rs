e670378096700653c027868a5aa11c9757f08765
Mitchell Skaggs <skaggsm333@gmail.com>
2019-04-21 20:33:56 -0500

    Add more precise values for Earth radius

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,9 @@ pub enum Units {
 }
 
 pub fn distance(start: Location, end: Location, units: Units) -> f64 {
-    const KILOMETERS: f64 = 6371.0;
-    const MILES: f64 = 3960.0;
+    /// Values from Moritz, H. Journal of Geodesy (2000) 74: 128. https://doi.org/10.1007/s001900050278
+    const KILOMETERS: f64 = 6371.0087714;
+    const MILES: f64 = 3958.76131603933;
     let r: f64;
 
     match units {
@@ -37,11 +38,11 @@ mod tests {
 
     #[test]
     fn haversine_distance_in_miles() {
-        assert_eq!(0.341336828310639, distance(Location { latitude: 38.898556, longitude: -77.037852 }, Location { latitude: 38.897147, longitude: -77.043934 }, Units::Miles));
+        assert_eq!(0.3412300584989182, distance(Location { latitude: 38.898556, longitude: -77.037852 }, Location { latitude: 38.897147, longitude: -77.043934 }, Units::Miles));
     }
 
     #[test]
     fn haversine_distance_in_kilometers() {
-        assert_eq!(0.5491557912038084, distance(Location { latitude: 38.898556, longitude: -77.037852 }, Location { latitude: 38.897147, longitude: -77.043934 }, Units::Kilometers));
+        assert_eq!(0.549156547264883, distance(Location { latitude: 38.898556, longitude: -77.037852 }, Location { latitude: 38.897147, longitude: -77.043934 }, Units::Kilometers));
     }
 }
